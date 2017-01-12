c5b96a8bd515638c5ac4a4d29e25d90f38fe8158
solomon123 <solomonyif@gmail.com>
2017-01-12 13:19:06 -0500

    so change on .tpl

--- sites/all/themes/ethiodiaspora/layouts/internal/internal-layout.tpl.php
+++ sites/all/themes/ethiodiaspora/layouts/internal/internal-layout.tpl.php
@@ -32,6 +32,7 @@
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if ($title): ?>
+        
         <h1><?php //print $title; ?></h1>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
